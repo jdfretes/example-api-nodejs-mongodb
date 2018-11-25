b2772df4a2622632db8e25e786c1433f19d0b3a2
fretesjuan4 <fretesjuan4@gmail.com>
2018-11-25 20:34:38 -0300

    primwere push

---
+++ index.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var cors = require('cors')
+var io = require('socket.io')(http);
+app.use(cors())
+var mensajes = []
+
+app.get('/', function (req, res) {
+    res.send('<h1>twApi</h1>');
+});
+
+io.on('connection', function (socket) {
+    socket.on('user new', function (msg) {
+        let customMsg = {
+            user: msg,
+            msg: 'Hola me conecte...'
+        }
+        console.log(customMsg.user + ' se conecto');
+        addMsg(customMsg)
+    });
+
+    socket.on('chat message', function (msg) {
+        addMsg(msg)
+    });
+});
+
+function addMsg(msg) {
+    mensajes.push(msg)
+    io.emit('send msg', mensajes)
+}
+
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
