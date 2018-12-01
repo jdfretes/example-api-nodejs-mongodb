a0a47ae191434fb70fe0cc79c4cad007477abc45
fretesjuan4 <fretesjuan4@gmail.com>
2018-12-01 16:54:29 -0300

    guardamos por el momento

--- index.js
+++ index.js
@@ -1,16 +1,67 @@
-var app = require('express')();
+'use strict'
+const app = require('express')();
+const bodyParser = require('body-parser');
 var http = require('http').Server(app);
+var jwt = require('jsonwebtoken');
 var cors = require('cors')
 var io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000
+
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 var mensajes = []
 
-app.get('/', function (req, res) {
-    res.send('<h1>twApi</h1>');
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>twApi</h1>');
 });
 
-io.on('connection', function (socket) {
-    socket.on('user new', function (msg) {
+app.get('/api', (req, res) => {
+    res.status(200).send({ message: 'root api' })
+});
+
+app.get('/api/product', (req, res) => {
+    res.status(200).send({ message: 'get productos' })
+});
+
+app.get('/api/product/:productId', (req, res) => {
+    res.status(200).send({ message: 'get producto' })
+});
+
+app.post('/api/product', (req, res) => {
+    res.status(200).send(req.body)
+});
+
+app.put('/api/product/productId', (req, res) => {
+    res.status(200).send({ message: 'put producto' })
+});
+
+app.delete('/api/product/productId', (req, res) => {
+    res.status(200).send({ message: 'delete producto' })
+});
+
+
+/*app.get('/api/login', (req, res) => {
+    //auth user
+    const user = {
+        id: 3,
+        user_name: 'juanda'
+    };
+    const token = jwt.sign({user}, 'my_secret_key')
+    res.json({
+        token: token
+    });
+});
+
+app.get('/api/protected', (req, res) => {
+    res.json({
+        text: 'api protected'
+    })
+});
+//sockets
+io.on('connection', (socket) => {
+    socket.on('user new', (msg) => {
         let customMsg = {
             user: msg,
             msg: 'Hola me conecte...'
@@ -19,7 +70,7 @@ io.on('connection', function (socket) {
         addMsg(customMsg)
     });
 
-    socket.on('chat message', function (msg) {
+    socket.on('chat message', (msg) => {
         addMsg(msg)
     });
 });
@@ -27,9 +78,9 @@ io.on('connection', function (socket) {
 function addMsg(msg) {
     mensajes.push(msg)
     io.emit('send msg', mensajes)
-}
+}*/
 
-
-http.listen(process.env.PORT || 3000, function () {
-    console.log('listening on *:3000');
+//server
+http.listen(PORT, () => {
+    console.log('Corriendo en el puerto: ', PORT);
 });
