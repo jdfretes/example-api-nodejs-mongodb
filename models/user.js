691493a214dfc995f312ceb495051fc2d2e3dfdb
Juan Fretes <fretesjuan4@gmail.com>
2018-12-02 18:08:45 -0300

    varias cosas

---
+++ models/user.js
@@ -0,0 +1,36 @@
+'use strict'
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
+
+const UserSchema = Schema({
+    email: { type: String, unique: true, lowercase: true },
+    displayName: String,
+    avatar: String,
+    password: { type: String, select: false },
+    signUpDate: { type: Date, default: Date.now() },
+    lastLogin: Date
+});
+
+UserSchema.pre('save', function (next) {
+    //if (!this.isModified('password')) return next()
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err)
+        bcrypt.hash(this.password, salt, null, (err, hash) => {
+            if (err) return next(err)
+            this.password = hash
+            next()
+        })
+    })
+})
+
+UserSchema.methods.gravatar = function () {
+    if (!this.email) return 'https://gravatar.com/avatar/?s=2006d-retro'
+
+    const md5 = crypto.createHash('md5').update(this.email).digest('hex')
+    return 'https://gravatar.com/avatar/' + md5 + '?s=200&d-retro'
+}
+
+
+module.exports = mongoose.model('User', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 'use strict'
 const express = require('express');
 const api = express.Router()
+const auth = require('../middlewares/auth');
 const ProductCtrl = require('../controllers/product');
+const UserCtrl = require('../controllers/user');
 
 api.get('/product', ProductCtrl.getProducts);
 api.get('/product/:productId', ProductCtrl.getProduct);
 api.post('/product', ProductCtrl.saveProduct);
 api.put('/product/:productId', ProductCtrl.updateProduct);
 api.delete('/product/:productId', ProductCtrl.deleteProduct);
+api.post('/signUp', UserCtrl.signUp);
+api.post('/signIn',auth, UserCtrl.signIn);
+api.get('/private', auth, (req, res) => {
+    res.status(200).send({ message: 'Tienes acceso...' })
+});
 
 module.exports = api
