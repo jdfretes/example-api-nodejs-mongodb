abc01beda928311b8bbc2527ed56a338cc3e0dce
Juan Fretes <fretesjuan4@gmail.com>
2018-12-08 20:00:04 -0300

    login ok

--- controllers/user.js
+++ controllers/user.js
@@ -7,7 +7,8 @@ const bcrypt = require('bcrypt-nodejs');
 function signUp(req, res) {
     const user = new User({
         email: req.body.email,
-        displayName: req.body.displayName
+        displayName: req.body.displayName,
+        password: req.body.password
     })
 
     user.avatar = user.gravatar()
@@ -20,26 +21,28 @@ function signUp(req, res) {
 
 function signIn(req, res) {
     console.log(req.body)
-    User.findOne({ email: req.body.email }).select('+password').then((user) => {
+    User.findOne({ email: req.body.email }).select('password').then((user) => {
         if (!user) return res.status(404).send({ message: 'No esxiste usuario con el email especificado...' })
-        console.log(req.body.password, user.password)
         //usamos hashSync porque cuando guardamos el password elegimos eso
-        var passwordIsValid = bcrypt.hashSync(req.body.password, user.password);
-        console.log(' passwordIsValid ', passwordIsValid)
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
         if (!passwordIsValid) return res.status(401).send({ auth: false, token: null, message: 'Contrasenha invalida' });
 
         res.status(200).send({
             message: 'Te has logueado correctamente...',
             token: service.createToken(user),
-            auth: true
+            auth: passwordIsValid
         })
     }).catch((err) => {
-        if (err) return res.status(500).send({ message: 'Disculpe los inconvenientes...' })
+        if (err) return res.status(500).send({ message: 'Disculpe los inconvenientes...'})
     });
-    
+
 }
 
 module.exports = {
     signUp,
     signIn
 }
+
+
+
+
