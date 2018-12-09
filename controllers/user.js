c6fb074f02aa75c9efbefada12b11fc60801bd5d
Juan Fretes <fretesjuan4@gmail.com>
2018-12-08 23:24:55 -0300

    fix de auth

--- controllers/user.js
+++ controllers/user.js
@@ -14,26 +14,28 @@ function signUp(req, res) {
     user.avatar = user.gravatar()
 
     user.save((err) => {
-        if (err) res.status(500).send({ message: 'Error al crear el usuario...' + err })
+        if (err) return res.status(500).send({ message: 'Error al crear el usuario...', error: err.code})
         return res.status(200).send({ token: service.createToken(user) })
     })
 }
 
 function signIn(req, res) {
-    console.log(req.body)
+    //si llega vacio el request rechazamos
+    if (Object.keys(req.body).length === 0) return res.status(404).send({ message: 'Debe enviar parametros(gil de mierda)...' })
+    //si llegan los parametros buscamos en la db
     User.findOne({ email: req.body.email }).select('password').then((user) => {
         if (!user) return res.status(404).send({ message: 'No esxiste usuario con el email especificado...' })
         //usamos hashSync porque cuando guardamos el password elegimos eso
         var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
         if (!passwordIsValid) return res.status(401).send({ auth: false, token: null, message: 'Contrasenha invalida' });
-
+        //si pass es valido damos aceso y un nuevo token
         res.status(200).send({
             message: 'Te has logueado correctamente...',
             token: service.createToken(user),
             auth: passwordIsValid
         })
     }).catch((err) => {
-        if (err) return res.status(500).send({ message: 'Disculpe los inconvenientes...'})
+        if (err) return res.status(500).send({ message: 'Disculpe los inconvenientes...' })
     });
 
 }
--- services/index.js
+++ services/index.js
@@ -7,7 +7,7 @@ function createToken(user) {
     const payload = {
         sub: user._id,
         iat: moment().unix(),
-        exp: moment().add(14, 'days').unix(),
+        exp: moment().add(1, 'days').unix(),
     }
 
     return jwt.encode(payload, config.SECRET_TOKEN)
@@ -25,9 +25,10 @@ function decodeToken(token) {
             }
             resolve(payload.sub)
         } catch (error) {
+            console.log(error)
             reject({
                 status: 500,
-                message: 'Invalid token'
+                message: 'Token invalido, o expirado'
             })
         }
     })
