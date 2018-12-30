1bd8280c807c50a47be0ec77cd7fb9d2e7d7d0a2
juanda <Juan Fretes>
2018-12-29 22:46:35 -0300

    validaciondes de registro mas mensajes de errores

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,14 @@ const service = require('../services/index');
 const bcrypt = require('bcrypt-nodejs');
 
 function signUp(req, res) {
+    if(req.body.email == "" || req.body.displayName == "" || req.body.password == "" ){
+        return res.status(404).send({ message: 'Debe llenar los datos requeridos...' })
+    }
+
+    if (!validateEmail(req.body.email)) {
+        return res.status(404).send({ message: 'Ingrese un email valido...' })
+    }
+
     const user = new User({
         email: req.body.email,
         displayName: req.body.displayName,
@@ -14,14 +22,19 @@ function signUp(req, res) {
     user.avatar = user.gravatar()
 
     user.save((err) => {
-        if (err) return res.status(500).send({ message: 'Error al crear el usuario...', error: err.code})
+        if (err) return res.status(500).send({ message: 'No se pudo reqistrar(verifique sus datos)...', error: err.code})
         return res.status(200).send({ token: service.createToken(user) })
     })
 }
 
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
 function signIn(req, res) {
     //si llega vacio el request rechazamos
-    if (Object.keys(req.body).length === 0) return res.status(404).send({ message: 'Debe enviar parametros(gil de mierda)...' })
+    if (Object.keys(req.body).length === 0) return res.status(404).send({ message: 'Debe llenar los datos requeridos...' })
     //si llegan los parametros buscamos en la db
     User.findOne({ email: req.body.email }).select('password').then((user) => {
         if (!user) return res.status(404).send({ message: 'No esxiste usuario con el email especificado...' })
